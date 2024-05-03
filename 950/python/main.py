b53a0b8b79a5417453b1e8d5a2fdd296263905d1
dachanh <ghostcodert7@gmail.com>
2024-05-03 11:43:35 +0700

    leetcode 950

---
+++ 950/python/main.py
@@ -0,0 +1,15 @@
+from collections import deque
+
+class Solution:
+    def deckRevealedIncreasing(self, deck: List[int]) -> List[int]:
+        N = len(deck)
+        sorted_deck = sorted(deck)
+        index = deque(range(N))
+        result = [None] * N
+
+        for card in sorted_deck:
+            result[index.popleft()] = card
+            if index:
+                index.append(index.popleft())
+
+        return result
