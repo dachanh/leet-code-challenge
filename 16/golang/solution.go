c49e5f69c3e61ea51e3e4cea7be660a46f97d80a
dachanh <ghostcodert7@gmail.com>
2026-05-13 16:19:00 +0700

    chore: resolve 16

--- 16/golang/solution.go
+++ 16/golang/solution.go
@@ -1,5 +1,7 @@
 package problem16
 
+import "slices"
+
 // 16. 3Sum Closest
 // Difficulty: Medium
 // Pattern:    fix+scan (two pointers)
@@ -8,5 +10,32 @@ package problem16
 // TODO: implement solution
 
 func threeSumClosest(nums []int, target int) int {
-	return 0
+	slices.Sort(nums)
+	closest := nums[0] + nums[1] + nums[2]
+	for i := 0; i < len(nums)-2; i++ {
+		l, r := i+1, len(nums)-1
+		for l < r {
+			sum := nums[i] + nums[l] + nums[r]
+
+			if abs(target-sum) < abs(target-closest) {
+				closest = sum
+			}
+			switch {
+			case sum < target:
+				l++
+			case sum > target:
+				r--
+			case sum == target:
+				return target
+			}
+		}
+	}
+	return closest
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
 }
